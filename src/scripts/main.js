523659720098df8d6a8bf78a94931fbb59b67111
9ravity <vinary_sys@naver.com>
2021-07-16 20:01:49 +0900

    last

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
      $('#lightgallery').lightGallery({
         selector: '.light-link',
         thumbnail: true,
-        download:false,
+        download: false,
     }); 
 
-    loadItemsInView();
+    //loadItemsInView();
 
     /*     preloading([
       "file:///C:/Users/shego/Desktop/m_wedding/weddinghall_sb_hj/src/images/section1.jpg",
@@ -142,27 +142,26 @@ clipboard.on( 'success', function() {
     },1500);
  } );
 
- function isElementInViewport(el) {
-/* sleep */
-    if (!el)
-      return false;
-    var rect = el.getBoundingClientRect();
-    rect.top >=0
-    console.log(rect.top >= 0)
-    rect.left >= 0
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth);
-    return true;
-  }
+//  function isElementInViewport(el) {
+//     if (!el)
+//       return false;
+//     var rect = el.getBoundingClientRect();
+//     rect.top >=0
+//     console.log(rect.top >= 0)
+//     rect.left >= 0
+//     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
+//     rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+//     return true;
+//   }
 
-  function loadItemsInView() {
-    //Select elements by the row id.
-    $("#lightgallery [data-src]").each(function () {
-        var isVisible = isElementInViewport(this);
-        if (isVisible) {
-            if ($(this).attr("src") == undefined) {
-                $(this).attr("src", $(this).data("src"));
-            }
-        }
-    });
-  }
+//   function loadItemsInView() {
+//     //Select elements by the row id.
+//     $("#lightgallery [data-src]").each(function () {
+//         var isVisible = isElementInViewport(this);
+//         if (isVisible) {
+//             if ($(this).attr("src") == undefined) {
+//                 $(this).attr("src", $(this).data("src"));
+//             }
+//         }
+//     });
+//   }
